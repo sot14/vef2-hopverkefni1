a87eaa90e884e90ef268849e57b69a96d187f800
sothorsteins <sothorsteins@gmail.com>
2021-03-13 18:42:16 +0000

    reading csv files

--- src/tv.js
+++ src/tv.js
@@ -1,11 +1,24 @@
 import fs from 'fs';
+import csv from 'csv-parser';
 
-const read = fs.createReadStream('./data/series.csv');
-
+const read = fs.createReadStream('./data/series.csv', {encoding: 'utf8'});
+let data = [];
+read.pipe(csv({headers: false, seperator: ','}));
 read.on('data', (chunk) => {
+    let item = {
+        bla: chunk[0],
+        blabla: chunk[1],
+        blablabla: chunk[2]
+    }
+    data.push(item);
   console.log('chunk', chunk);
 });
 
 read.on('close', () => {
   console.log('file read');
 });
+
+read.on('end', () => {
+    console.log("data", data);
+    return data;
+})
