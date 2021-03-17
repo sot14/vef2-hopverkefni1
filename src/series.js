40cae473a66266726e64eadc7f6e9365a49468f3
Jenný Hildur Ómarsdóttir <jennyhildur@Jennys-MacBook-Pro.local>
2021-03-17 18:34:38 +0000

    route

--- src/series.js
+++ src/series.js
@@ -1,6 +1,29 @@
-import xss from 'xss';
-//import { addPageMetadata } from './utils.js';
-//import { query, pagedQuery } from '../utils/db';
-
-
-// plokka series úr tv.js til að gera birt undir /tv/:id, /tv/:id/rate, /tv/:id/state
+    const xss = require('xss');
+    import { addPageMetadata } from './utils.js';
+    const { query, pagedQuery } = require('../utils/db');
+    import {
+       isInt,
+       isEmpty,
+       isNotEmptyString,
+       isString,
+       toPositiveNumberOrDefault,
+       lengthValidationError
+     } from '.,/authentication/validations.js'
+     
+    // plokka series úr tv.js til að gera birt undir /tv/:id, /tv/:id/rate, /tv/:id/state
+    export async function listCategories(req, res) {
+       const { offset = 0, limit = 10 } = req.query;
+        const series = await pagedQuery(
+         'SELECT id, name FROM series ORDER BY aired DESC',
+         [],
+         { offset, limit },
+       );
+        const seriesWithPage = addPageMetadata(
+           series,
+         req.path,
+         { offset, limit, length: series.items.length },
+       );
+        return res.json(seriesWithPage);
+     }
+    
+    
