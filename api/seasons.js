01788a78a0b57c45daa9cf710da02e28b5d46ecc
sothorsteins <sothorsteins@gmail.com>
2021-03-20 18:04:28 +0000

    Drop and create tables in setup

--- api/seasons.js
+++ api/seasons.js
@@ -23,3 +23,39 @@ export async function listSeasons(req, res) {
       );
       return res.json(seasonsWithPage);
 }
+
+// Birtir upplýsingar um staka seríu
+export async function listSeason(req, res) {
+    const { id } = req.params;
+    console.log("list seaosns", req.params);
+    console.log("body", req);
+    const season = await findSeason(id);
+  
+    if (!season) {
+      return res.status(404).json({ error: 'Season not found' });
+    }
+  
+    return res.json(season);
+  }
+  
+  // Finnur seríu út frá id 
+  export async function findSeason(id) {
+    if (!isInt(id)) {
+      return null;
+    }
+  
+    const season = await query(
+      `SELECT
+      *
+      FROM
+        season
+      WHERE id = $1`,
+      [id],
+    );
+  
+    if (season.rows.length !== 1) {
+      return null;
+    }
+  
+    return season.rows[0];
+  }
